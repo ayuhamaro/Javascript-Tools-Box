8220eb071eeb9e25213964bec483810d8ae94e5c
Maro Huang <ayuhamaro@gmail.com>
2012-12-25 15:39:58 +0800

    HTML tags dynamic binding event (Javascript Closure)
    In this example, the demonstration using jQuery selector to find the
    specified HTML tags (such as A tag).Bind all A tag each click event, and
    cover the href attribute value.

---
+++ html-tags-dynamic-binding-event.js
@@ -0,0 +1,14 @@
+﻿var linkCount = $('a').length;
+if(linkCount > 0){
+    for(i = 0; i < linkCount; i++){
+        var objName = 'a_obj_id_' + i;    //Combination of the id attribute
+        $('a')[i].id = objName;
+        (function(name){    //Anonymous Function
+            $('#' + name).bind('click', function(){  //Tag id attribute event binding
+                alert(name);
+                return false;
+            });
+        })(objName);   //Executive function and passing the tag id attribute
+        $('a')[i].href = '#';    //Cover tag href attribute
+    }
+}
